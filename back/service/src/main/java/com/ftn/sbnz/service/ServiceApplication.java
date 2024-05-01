1658d8e536d181a71d523b08e077922f20106d1b
Nada Zaric <nada0zaric@gmail.com>
2024-05-01 12:11:24 +0200

    Initial commit [BACK]

---
+++ back/service/src/main/java/com/ftn/sbnz/service/ServiceApplication.java
@@ -0,0 +1,33 @@
+package com.ftn.sbnz.service;
+
+import org.springframework.context.annotation.Bean;
+import org.kie.api.KieServices;
+import org.kie.api.builder.KieScanner;
+import org.kie.api.runtime.KieContainer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceApplication  {
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceApplication.class, args);
+	}
+
+	@Bean
+	public KieContainer kieContainer() {
+		KieServices ks = KieServices.Factory.get();
+		KieContainer kContainer = ks
+				.newKieContainer(ks.newReleaseId("com.ftn.sbnz", "kjar", "0.0.1-SNAPSHOT"));
+		KieScanner kScanner = ks.newKieScanner(kContainer);
+		kScanner.start(1000);
+		return kContainer;
+	}
+	
+	/*
+	 * KieServices ks = KieServices.Factory.get(); KieContainer kContainer =
+	 * ks.newKieContainer(ks.newReleaseId("drools-spring-v2",
+	 * "drools-spring-v2-kjar", "0.0.1-SNAPSHOT")); KieScanner kScanner =
+	 * ks.newKieScanner(kContainer); kScanner.start(10_000); KieSession kSession =
+	 * kContainer.newKieSession();
+	 */
+}
