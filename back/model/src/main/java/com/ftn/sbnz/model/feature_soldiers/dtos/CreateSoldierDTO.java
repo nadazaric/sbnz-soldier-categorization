c3b60c8e141bf2a899aa25cdc30fbf9ac990039c
Nada Zaric <nada0zaric@gmail.com>
2024-05-19 23:29:15 +0200

    Add: Create DTOs

--- back/model/src/main/java/com/ftn/sbnz/model/feature_soldiers/dtos/CreateSoldierDTO.java
+++ back/model/src/main/java/com/ftn/sbnz/model/feature_soldiers/dtos/CreateSoldierDTO.java
@@ -1,15 +1,22 @@
 package com.ftn.sbnz.model.feature_soldiers.dtos;
 
+import java.util.List;
+
 public class CreateSoldierDTO {
     
     private String fullName;
     private String jmbg;
+    private List<CreateWarDutyDTO> warDuties;
+    private List<CreateInjuryDTO> injuries;
 
     public CreateSoldierDTO() {}
-    
-    public CreateSoldierDTO(String fullName, String jmbg) {
+
+    public CreateSoldierDTO(String fullName, String jmbg, List<CreateWarDutyDTO> warDuties,
+            List<CreateInjuryDTO> injuries) {
         this.fullName = fullName;
         this.jmbg = jmbg;
+        this.warDuties = warDuties;
+        this.injuries = injuries;
     }
 
     public String getFullName() {
@@ -23,5 +30,17 @@ public class CreateSoldierDTO {
     }
     public void setJmbg(String jmbg) {
         this.jmbg = jmbg;
+    }
+    public List<CreateWarDutyDTO> getWarDuties() {
+        return warDuties;
+    }
+    public void setWarDuties(List<CreateWarDutyDTO> warDuties) {
+        this.warDuties = warDuties;
+    }
+    public List<CreateInjuryDTO> getInjuries() {
+        return injuries;
+    }
+    public void setInjuries(List<CreateInjuryDTO> injuries) {
+        this.injuries = injuries;
     }
 }
