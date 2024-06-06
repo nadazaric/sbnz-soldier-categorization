e0bcdbded8ceee7eb803453952c36ccdd5888ef6
Nada Zaric <nada0zaric@gmail.com>
2024-06-06 12:50:07 +0200

    Add: Show soldiers in unit [front]

--- front/pages/units/index.js
+++ front/pages/units/index.js
@@ -1,20 +1,47 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { AddSoldier } from "@/components/AddSoldier"
+import { DialogWithHeader } from "@/components/Dialog"
+import TableSoldiers from "@/components/TableSoldiers"
 import { BACK_BASE_URL } from "@/helper/environment"
+import { getTranslation } from "@/locales/TranslationHelper"
 import axios from "axios"
 import { useEffect, useState } from "react"
 
 export default function Units() {
+    const t = getTranslation()
     const [soldiers, setSoldiers] = useState([])
-    const [selectedId, setSelectedId] = useState(3)
+    const [selectedUnitId, setSelectedId] = useState(2)
+    const [selectedSoldierId, setSelectedSoldierId] = useState(null);
+    const [openDialog, setOpenDialog] = useState(false)
 
     useEffect(() => {
-        axios.get(`${BACK_BASE_URL}/unit/soldiers-for-unit/${selectedId}`)
-            .then(response => {console.log(response.data); setSoldiers(response.data) })
+        axios.get(`${BACK_BASE_URL}/unit/soldiers-for-unit/${selectedUnitId}`)
+            .then(response => { setSoldiers(response.data) })
             .catch(_error => {})
     }, [])
 
+    function onSoldierClick(soldier) {
+        setSelectedSoldierId(soldier.id)
+        setOpenDialog(true)
+    }
+
     return(
-        <div>
-            {"Units"}
+        <div className="page">
+            <TableSoldiers 
+                soldiers={soldiers}
+                onSoldierClick={onSoldierClick}
+            />
+            <DialogWithHeader
+                isOpen={openDialog}
+                width={600}
+                onCloseModal={() => setOpenDialog(false)}
+                title={t.soldiers_add_title}
+            >
+                <AddSoldier 
+                    selectedId={selectedSoldierId}
+                    isOpen={openDialog}
+                />
+            </DialogWithHeader>
         </div>
     )
 }
