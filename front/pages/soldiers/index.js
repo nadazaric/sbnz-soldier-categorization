86e7c4fb45db2ea426dffe657a70b79764871d6f
Nada Zaric <nada0zaric@gmail.com>
2024-06-06 17:23:21 +0200

    Add: Show soldiers at same page

--- front/pages/soldiers/index.js
+++ front/pages/soldiers/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { DialogWithHeader } from "@/components/Dialog";
 import { ButtonHeader } from "@/components/Header";
 import { AddDetailsSoldier } from "@/components/AddSoldier";
@@ -6,6 +7,7 @@ import { BACK_BASE_URL } from "@/helper/environment";
 import { getTranslation } from "@/locales/TranslationHelper";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { DropdownMenuOption } from "@/components/DropdownMenu";
 
 export default function Soldiers() {
     const t = getTranslation()
@@ -13,11 +15,32 @@ export default function Soldiers() {
     const [openDialog, setOpenDialog] = useState(false)
     const [selectedId, setSelectedId] = useState('')
 
+    // units
+    const [selectedUnitId, setSelectedUnitId] = useState(null)
+    const [unitOption, setUnitOptns] = useState([{value: '', text: ''}])
     useEffect(() => {
-        axios.get(`${BACK_BASE_URL}/soldier`)
-            .then(response => { setSoldiers(response.data) })
+        axios.get(`${BACK_BASE_URL}/unit/units-except-soldier`)
+            .then(response => { 
+                var options = []
+                options.push({value: null, text: "DEAFULT"})
+                for (var option of response.data) options.push({value: option.id, text: option.name})
+                setUnitOptns(options) 
+            })
             .catch(_error => {})
     }, [])
+
+    function getSoldiersForUnit() {
+        axios.get(`${BACK_BASE_URL}/unit/soldiers-for-unit/${selectedUnitId}`)
+            .then(response => {console.log('UNIT ' + selectedUnitId); setSoldiers(response.data) })
+            .catch(_error => {})
+    }
+
+    // soldiers
+    function getAllSoldiers() {
+        axios.get(`${BACK_BASE_URL}/soldier`)
+            .then(response => {console.log('ALL'); setSoldiers(response.data) })
+            .catch(_error => {})
+    }
 
     function onSoldierClick(soldier) {
         setSelectedId(soldier.id)
@@ -29,25 +52,41 @@ export default function Soldiers() {
         setSelectedId(null)
     }
 
-    const saveSoldier = async (formData) => {
-        try {
-            const response = await axios.post(`${BACK_BASE_URL}/soldier`, formData, {
-                headers: { 'Content-Type': 'application/json' }
-            })
-            if (response.status === 201) {
-                setSoldiers(prevSoldiers => [...prevSoldiers, response.data])
-                setOpenDialog(false)
-            }
-            else { }
-        } catch (error) { }
+    // globals
+    useEffect(() => {
+        getSoldiers()
+    }, [selectedUnitId])
+
+    function getSoldiers() {
+        if (selectedUnitId == null) getAllSoldiers()
+        else getSoldiersForUnit()
     }
+
+    const saveSoldier = (formData) => {
+        axios.post(`${BACK_BASE_URL}/soldier`, formData, {
+            headers: { 'Content-Type': 'application/json' }
+        })
+        .then(response => {
+            if (response.status === 201) {
+                setOpenDialog(false)
+                setSelectedUnitId(null)
+            }
+        })
+        .catch(error => {});
+    };
 
     return(
         <div className='page'>
             <ButtonHeader 
                 title={t.soldiers_header_title}
                 onAddClick={onAddClick} 
-            />
+            >
+                <DropdownMenuOption
+                    options={unitOption}
+                    selectedDefault={selectedUnitId}
+                    onSelect={(e) => setSelectedUnitId(e.value)}
+                />
+            </ButtonHeader>
             <div className="spacer_hor_M" />
             <TableSoldiers 
                 soldiers={soldiers} 
