64abbba3a25259c315e2af383d4923c0e2282a84
Nada Zaric <nada0zaric@gmail.com>
2024-06-07 16:16:57 +0200

    Add: Competitions data to data.sql

--- back/service/src/main/resources/data.sql
+++ back/service/src/main/resources/data.sql
@@ -24,6 +24,14 @@ INSERT INTO UNIT(name, parent_id, type) VALUES ('VII Jedinica', 7, 1);
 INSERT INTO UNIT(name, parent_id, type) VALUES ('VIII Jedinica', 7, 1);
 
 INSERT INTO SPA_COMPETITION(name, year, is_done, position_number) VALUES ('Konkurs za 2023 godinu', '2023', true, 2);
+INSERT INTO COMPETITOR(full_name, jmbg, status, dead_family_member, injury_type, score) VALUES ('Milovan Djuric', '1', 2, 2, 1, 20);
+INSERT INTO COMPETITOR(full_name, jmbg, status, dead_family_member, injury_type, score) VALUES ('Marko Peric', '2', 2, 1, 1, 25);
+INSERT INTO COMPETITOR(full_name, jmbg, status, dead_family_member, injury_type, score) VALUES ('Nada Zaric', '3', 2, 1, 2, 30);
+INSERT INTO SPA_COMPETITION_COMPETITORS(spa_competition_id, competitors_id) VALUES (1, 1);
+INSERT INTO SPA_COMPETITION_COMPETITORS(spa_competition_id, competitors_id) VALUES (1, 2);
+INSERT INTO SPA_COMPETITION_COMPETITORS(spa_competition_id, competitors_id) VALUES (1, 3);
+INSERT INTO SPA_COMPETITION_SELECTED_COMPETITORS(spa_competition_id, selected_competitors_id) VALUES (1, 2);
+INSERT INTO SPA_COMPETITION_SELECTED_COMPETITORS(spa_competition_id, selected_competitors_id) VALUES (1, 3);
 
 -- INSERT INTO SOLDIER (full_name, jmbg, months, category, monthly_contribution) VALUES ('Pera Peric', '076436335357', 0, 0, 0.0);
 -- INSERT INTO SOLDIER (full_name, jmbg, months, category, monthly_contribution) VALUES ('Marko Markovic', '076437565357', 0, 0, 0.0);
